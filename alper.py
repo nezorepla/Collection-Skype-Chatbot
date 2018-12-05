169f1ecf6279cb1bda90692a982a93892b14e740
nezorepla <alperozen84@gmail.com>
2018-12-05 16:07:41 +0300

    Update and rename main.py to alper.py

--- alper.py
+++ alper.py
@@ -2,23 +2,38 @@
 import os
 import vodaucwa
 
+import aiml
+
+# Create the kernel and learn AIML files
+kernel = aiml.Kernel()
+kernel.learn("std-startup.xml")
+kernel.respond("load aiml b")
+
+# Press CTRL-C to break this loop
+#while True:
+#    print kernel.respond(raw_input("Enter your message >> "))
+
+
 
 def messageReceived(message):
   #  message.reply("Selam %s, su an cevap veremesem de,bu mesaja en kisa zamanda cevap verecegim; \"%s\"." % (message.uri, message.message))
-  message.reply("OTOMATIK CEVAP: Selam, su an cevap veremesem de,bu mesaja en kisa zamanda cevap verecegim; <b>\"%s\"</b>" % ( message.message))
+  message.reply("OTOMATIK CEVAP:\"%s\"</b>" % (kernel.respond(message.message)))
 
-sicil=""
- 
+
 def main():
     bot = vodaucwa.LyncBot()
     bot.setMessageCallback(messageReceived)
-    username = "xxxx\\xxxxx"
-    password = "xxxxx"
+    username = "xxx\\xxx"
+    password = "xxx"
 #    proxy = "http://proxy.company.com:80/"
 #    os.environ["http_proxy"] = proxy
 #    os.environ["https_proxy"] = proxy
     bot.authenticate("https://lyncdiscoverinternal.xxx.com.tr/", username, password)
     bot.loop()
 
+
+
+
+    
 if __name__ == '__main__':
     main()
